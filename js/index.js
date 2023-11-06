c0f43d2e9908de1c5199feeed3c3eb4b5da6ddd1
IK <kodai0225.yuru@icloud.com>
2023-11-06 16:50:47 +0900

    change html js css

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-$(function () {
-    $("h1").textillate();
-});
+// $(function () {
+//     $("h1").textillate();
+// });
 
-document.getElementById("date").innerHTML = getNow();
+// document.getElementById("date").innerHTML = getNow();
 
-function getNow() {
-    var now = new Date();
-    var year = now.getFullYear();
-    var mon = now.getMonth() + 1;
-    var day = now.getDate();
-    
-    var nowDate = year + "/" + mon + "/" + day;
+// function getNow() {
+//     var now = new Date();
+//     var year = now.getFullYear();
+//     var mon = now.getMonth() + 1;
+//     var day = now.getDate();
 
-    return nowDate;
-}
+//     var nowDate = year + "/" + mon + "/" + day;
+
+//     return nowDate;
+// }
 
