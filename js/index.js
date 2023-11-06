826e7656ddf609987dc04b16590db1bbba5b6fa1
IK <kodai0225.yuru@icloud.com>
2023-11-06 14:37:23 +0900

    dev first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("h1").textillate();
+});
+
+document.getElementById("date").innerHTML = getNow();
+
+function getNow() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var mon = now.getMonth() + 1;
+    var day = now.getDate();
+    
+    var nowDate = year + "/" + mon + "/" + day;
+
+    return nowDate;
+}
+
