8cadd470871ce782849bd57b5d194a86c5ded43c
IK <kodai0225.yuru@icloud.com>
2023-11-08 13:17:32 +0900

    [add] 文字数カウント機能追加

--- js/index.js
+++ js/index.js
@@ -15,3 +15,22 @@
 //     return nowDate;
 // }
 
+function countCharacters() {
+    var inputElement = document.getElementById('inputText');
+    var characterCountElement = document.getElementById('characterCount');
+    var characterCountNSElement = document.getElementById('characterCountNS');
+    
+    var inputText = inputElement.value;
+    // 改行を削除
+    var inputTextMain = inputText.replace( /\n+/g,"");
+    //改行とスペースを削除
+    var inputTextNS = inputTextMain.replace(/ +/g,"");
+    inputTextNS = inputTextNS.replace(/　+/g,"");
+    // 文字数計算
+    var characterCount = inputTextMain.length;
+    var characterCountNS = inputTextNS.length;
+    //画面出力
+    characterCountElement.textContent = characterCount;
+    characterCountNSElement.textContent = characterCountNS;
+}
+
